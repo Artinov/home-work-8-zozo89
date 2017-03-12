f833193752d8df8a18cf891acf9a7d96c543b96a
zozo89 <v.prinko@yandex.ru>
2017-03-12 13:43:36 +0200

    did all tasks

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 function setText(selector, text) {
-	/*
-		`selector` is string with CSS-like selector
-		`text` - is just string with text we want to change
-	*/
-
-	// Write your code here
+	var element = document.querySelector(selector);
+		element.innerHTML = text;
 }
-
 
 function setTextAll(selector, text) {
-	// Write your code here
+	var elements = document.querySelectorAll(selector);
+		elements.forEach(function(s){
+			s.innerHTML = text;
+		});
 }
-
 
 setText("p", "wasn't that hard, yeah ?");
 setTextAll("div .block a", "or was it ?");
